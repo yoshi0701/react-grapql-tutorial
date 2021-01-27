02b687ac1406bdd68a3f2cc133a787e4fbe15787
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-01-27 23:37:04 +0900

    feat: replacing const data for gql via useQuery

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,26 +1,35 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import Link from './Link';
 
-const LinkList = () => {
-  const linksToRender = [
-    {
-      id: '1',
-      description:
-       'Prisma gives you a powerful database toolkit 😎',
-      url: 'https://prisma.io'
-    },
-    {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+      }
     }
-  ];
+  }
+`;
+
+const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY);
 
   return (
     <div>
-      {linksToRender.map((link) => (
-        <Link key={link.id} link={link} />
-      ))}
+      {
+        data && (
+          <>
+            {data.feed.links.map((link) => (
+              <Link key={link.id} link={link} />
+            ))}
+          </>
+        )
+      }
     </div>
   );
 };
