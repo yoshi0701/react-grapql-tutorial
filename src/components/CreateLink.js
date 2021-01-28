9b6a71cedbc39e8b3cedde1bfb7e23ba2358da68
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-01-28 20:52:50 +0900

    feat: createLink updated for submitting

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation, gql } from '@apollo/client';
+import { useHistory } from 'react-router';
 
 
 const CREATE_LINK_MUTATION = gql`
@@ -16,6 +17,7 @@ const CREATE_LINK_MUTATION = gql`
 `;
 
 const CreateLink = () => {
+  const history = useHistory();
   const [formState, setFormState] = useState({
     description: '',
     url: ''
@@ -25,7 +27,8 @@ const CreateLink = () => {
     variables: {
       description: formState.description,
       url: formState.url
-    }
+    },
+    onCompleted: () => history.push('/')
   });
 
   return (
